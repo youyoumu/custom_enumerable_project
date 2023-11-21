3be455cb63b6684e45de60ad43aa35b7713d17da
yym2 <19760884+youyoumu@users.noreply.github.com>
2023-11-21 16:41:59 +0700

    Complete all assignment

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,94 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield self[i], i
+      i += 1
+    end
+    return self
+  end
+
+  def my_select
+    i = 0
+    new_array = []
+    while i < self.length
+      if yield self[i]
+        new_array << self[i]
+      end
+      i += 1
+    end
+    return new_array
+  end
+
+  def my_all?
+    i = 0
+    while i < self.length
+      unless yield self[i]
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+
+  def my_any?
+    i = 0
+    while i < self.length
+      if yield self[i]
+        return true
+      end
+      i += 1
+    end
+    return false
+  end
+
+  def my_none?
+    i = 0
+    while i < self.length
+      if yield self[i]
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+
+  def my_count
+    unless block_given?
+      return self.length
+    end
+    i = 0
+    counter = 0
+    while i < self.length
+      if yield self[i]
+        counter += 1
+      end
+      i += 1
+    end
+    return counter
+  end
+
+  def my_map
+    i = 0
+    new_array = []
+    while i < self.length
+      a = yield self[i]
+      new_array << a
+      i += 1
+    end
+    return new_array
+  end
+
+  def my_inject(initial_value)
+    i = 0
+    accumulator = initial_value
+    while i < self.length
+      accumulator = yield accumulator, self[i]
+      i += 1
+    end
+    return accumulator
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +97,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    while i < self.length
+      yield self[i]
+      i += 1
+    end
+    return self
+  end
 end
